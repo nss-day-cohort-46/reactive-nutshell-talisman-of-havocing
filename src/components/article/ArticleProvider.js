06a9124da670ab4afa4f553970560d1e52348d14
yasmeencole <yasmeencole1@mail.com>
2021-03-08 15:09:08 -0600

    created Article.css, Article.js, ArticleDetail.js, ArticleForm.js, ArticleList.js and ArticleProvider.js. added Article to database

---
+++ src/components/article/ArticleProvider.js
@@ -0,0 +1,71 @@
+import React, { useState, createContext } from "react"
+
+// The context is imported and used by individual components that need data
+export const ArticleContext = createContext()
+
+// This component establishes what data can be used.
+export const ArticleProvider = (props) => {
+    const [articles, setArticles] = useState([])
+
+    const [ searchTerms, setSearchTerms ] = useState("")
+
+    // useState([])  is to hold and set the array of article
+    // useState() hook to define a variable that holds the state of the component, and a function that updates it.
+
+    const getArticles = () => {
+    return fetch("http://localhost:8088/articles")
+        .then(response => response.json())
+        .then(articlesData => setArticles(articlesData))
+}
+
+const addArticle = articlesObj => {
+    return fetch("http://localhost:8088/articles", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(articlesObj)
+    })
+    .then(response => response.json())
+}
+
+
+const getArticleById = (id) => {
+    return fetch(`http://localhost:8088/articles/${id}?_expand=location&_expand=customer`)
+        .then(res => res.json())
+}
+
+const releaseArticle = articleId => {
+    return fetch(`http://localhost:8088/articles/${articleId}`, {
+        method: "DELETE"
+    })
+    .then(getArticles)
+}
+
+const updateArticle = article => {
+return fetch(`http://localhost:8088/articles/${article.id}`, {
+    method: "PUT",
+    headers: {
+    "Content-Type": "application/json"
+    },
+    body: JSON.stringify(article)
+})
+    .then(getArticles)
+}
+
+/*
+    You return a context provider which has the
+    `articles` state, `getArticles` function,
+    and the `addArticle` function as keys. This
+    allows any child elements to access them.
+  */
+    return (
+    <ArticleContext.Provider value={{
+    //   articles: articles, 
+    //   getArticles: getArticles
+        articles, getArticles, addArticle, getArticleById, releaseArticle, updateArticle, searchTerms, setSearchTerms
+    }}>
+        {props.children}
+        </ArticleContext.Provider>
+)
+}
