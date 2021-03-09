b37e1874824ac665a9fcf5076823ea1034f290a9
Nick Carver <nicholascarver2@outlook.com>
2021-03-09 14:45:33 -0600

    got the form to show up as a modal

--- src/components/events/EventForm.js
+++ src/components/events/EventForm.js
@@ -24,7 +24,7 @@ export const EventForm = () => {
         setEvent(newEvent)
     }
 
-    const handleClickSaveEmployee = (event) => {
+    const handleClicksSaveEvent = (event) => {
         event.preventDefault()
 
         setIsLoading(true)
@@ -36,7 +36,7 @@ export const EventForm = () => {
                 date: anEvent.date,
                 location: anEvent.location,
             })
-                .then(() => history.push(`/events/detail/${anEvent.id}`))
+                .then(() => history.push(`/events`))
         } else {
             addEvent(anEvent)
                 .then(() => history.push("/events"))
@@ -61,28 +61,32 @@ export const EventForm = () => {
     }, [])
 
     return (
-        <form className="eventForm">
-            <h2 className="eventForm__title">{eventId ? "Edit Event" : "Add Event"}</h2>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="name">Event name:</label>
-                    <input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="event name" value={anEvent.name} />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="role">Event Date:</label>
-                    <input type="datetime-local" id="date" onChange={handleControlledInputChange} required className="form-control" value={anEvent.date} />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="name">Event Location:</label>
-                    <input type="text" id="location" onChange={handleControlledInputChange} required className="form-control" placeholder="event location" value={anEvent.location} />
-                </div>
-            </fieldset>
-            <button disabled={isLoading} className="btn btn-primary" onClick={handleClickSaveEmployee}>{eventId ? "Submit Edit" : "Save New Event"}</button>
-            <button className="btn" onClick={handleCancel}>Cancel</button>
-        </form>
+        <div id="eventForm__modal" className="modal--parent">
+            <div className="modal--content">
+                <form className="eventForm">
+                    <h2 className="eventForm__title">{eventId ? "Edit Event" : "Add Event"}</h2>
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="name">Event name:</label>
+                            <input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="event name" value={anEvent.name} />
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="role">Event Date:</label>
+                            <input type="datetime-local" id="date" onChange={handleControlledInputChange} required className="form-control" value={anEvent.date} />
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="name">Event Location:</label>
+                            <input type="text" id="location" onChange={handleControlledInputChange} required className="form-control" placeholder="event location" value={anEvent.location} />
+                        </div>
+                    </fieldset>
+                    <button disabled={isLoading} className="btn btn-primary" onClick={handleClicksSaveEvent}>{eventId ? "Submit Edit" : "Save New Event"}</button>
+                    <button className="btn" onClick={handleCancel}>Cancel</button>
+                </form>
+            </div>
+        </div>
     )
 }
