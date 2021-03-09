1ea15606f47d3fed60721357afa870e7fd10d105
Nick Carver <nicholascarver2@outlook.com>
2021-03-09 13:54:24 -0600

    added an add event button that currently does nothing

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -6,7 +6,7 @@ import './Event.css'
 export const EventList = () => {
     const { events, getEvents } = useContext(EventContext)
 
-    const sortedEvents = events.slice().sort((a,b) => {
+    const sortedEvents = events.slice().sort((a, b) => {
         const aDate = new Date(a.date)
         const bDate = new Date(b.date)
         a.date = aDate
@@ -18,9 +18,16 @@ export const EventList = () => {
         getEvents()
     }, [])
 
+    const handleClickAddEvent = () => {
+        console.log("button clicked")
+    }
+
     return (
         <section className="events">
-            <h3 className="eventListH3">Upcoming Events</h3>
+            <div className="eventListHeader">
+                <h3 className="eventListH3">Upcoming Events</h3>
+                <button className="btn btn-primary newEventButton" onClick={handleClickAddEvent}>+ Event</button>
+            </div>
             {sortedEvents.map(eventObj => {
                 return <EventCard key={eventObj.id} eventObj={eventObj} />
             })}
