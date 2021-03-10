a7f6786bdb31e9a98a475a485d972516b54076f8
Nick Carver <nicholascarver2@outlook.com>
2021-03-10 14:29:08 -0600

    the top event is now larger and bolder and has a different background color. The delete buton is also on there

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -5,18 +5,37 @@ import { EventForm } from './EventForm'
 export const EventCard = ({ eventObj, eventCounter }) => {
     const theDate = new Date(eventObj.date)
     const eventDate = theDate.toLocaleDateString('en-US', { timeZone: "CST" })
-    const eventTime = theDate.toLocaleTimeString('en-US', { timeZone: "CST", hour: '2-digit', minute:'2-digit' })
-    const dayOfTheWeek = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const eventTime = theDate.toLocaleTimeString('en-US', { timeZone: "CST", hour: '2-digit', minute: '2-digit' })
+    const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const eventDay = dayOfTheWeek[theDate.getDay()]
     const [openForm, setOpenForm] = useState(false)
-    let inputStyle = {}
+    const loggedInUserId = parseInt(sessionStorage.getItem("nutshell_user"))
+    let headerInputStyle = {}
+    let paragraphInputStyle = {}
+    let sectionInputStyle = {}
+    let buttonInputStyle = {}
+    let editButton = false
     if (eventCounter === 1) {
-        
+        headerInputStyle = {
+            fontWeight: 'bold',
+            fontSize: 'xx-large',
+            marginTop: '10px',
+        }
+        paragraphInputStyle = {
+            fontWeight: 'bold',
+            fontSize: 'large',
+        }
+        sectionInputStyle = {
+            backgroundColor: 'rgb(129, 190, 129)',
+            borderRadius: '20px',
+            padding: '10px',
+            marginTop: '10px',
+        }
+        buttonInputStyle = {
+            backgroundColor: 'rgb(129, 190, 129)',
+        }
     }
 
-
-    const loggedInUserId = parseInt(sessionStorage.getItem("nutshell_user"))
-    let editButton = false
     if (loggedInUserId === eventObj.userId) {
         editButton = true
     }
@@ -24,15 +43,21 @@ export const EventCard = ({ eventObj, eventCounter }) => {
     const handleClickAddEvent = () => {
         setOpenForm(true)
     }
+    const handleClickDeleteEvent = () => {
+        setOpenForm(true)
+    }
 
     return (
         <>
-            <section>
-                <h4 style={inputStyle} className="eventCardH4">{eventObj.name}</h4>
-                <p className="eventCardP">{eventDay} - {eventDate}</p>
-                <p className="eventCardP">{eventTime} @ {eventObj.location}</p>
-                {editButton ? <button className="editEventButton" onClick={handleClickAddEvent}>edit</button> : ""}
-                {openForm && <EventForm  setOpenForm={setOpenForm} eventId={eventObj.id}/>}
+            <section style={sectionInputStyle}>
+                <h4 style={headerInputStyle} className="eventCardH4">{eventObj.name}</h4>
+                <p style={paragraphInputStyle} className="eventCardP">{eventDay} - {eventDate}</p>
+                <p style={paragraphInputStyle} className="eventCardP">{eventTime} @ {eventObj.location}</p>
+                <div>
+                    {editButton ? <button style={buttonInputStyle} className="editEventButton" onClick={handleClickAddEvent}>edit</button> : ""}
+                    {editButton ? <button style={buttonInputStyle} className="deleteEventButton" onClick={handleClickDeleteEvent}>delete</button> : ""}
+                </div>
+                {openForm && <EventForm setOpenForm={setOpenForm} eventId={eventObj.id} />}
             </section>
         </>
     )
