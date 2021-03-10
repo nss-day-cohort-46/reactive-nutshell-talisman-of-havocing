f52b20c51467638b8d5031e36306aef6a0ca37a4
Nick Carver <nicholascarver2@outlook.com>
2021-03-10 10:36:28 -0600

    added an edit button to each event. Got the form to pull up when the edit button is clicked. added styling to the button

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import './Event.css'
+import { EventForm } from './EventForm'
 
 export const EventCard = ({ eventObj }) => {
     const theDate = new Date(eventObj.date)
@@ -6,12 +8,20 @@ export const EventCard = ({ eventObj }) => {
     const eventTime = theDate.toLocaleTimeString('en-US', { timeZone: "CST", hour: '2-digit', minute:'2-digit' })
     const dayOfTheWeek = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const eventDay = dayOfTheWeek[theDate.getDay()]
+    const [openForm, setOpenForm] = useState(false)
+
+    const handleClickAddEvent = () => {
+        setOpenForm(true)
+    }
+
     return (
         <>
             <section>
                 <h4 className="eventCardH4">{eventObj.name}</h4>
                 <p className="eventCardP">{eventDay} - {eventDate}</p>
                 <p className="eventCardP">{eventTime} @ {eventObj.location}</p>
+                <button className="editEventButton" onClick={handleClickAddEvent}>edit</button>
+                {openForm && <EventForm  setOpenForm={setOpenForm} eventId={eventObj.id}/>}
             </section>
         </>
     )
